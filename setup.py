c0c076eb32af414f624c2f2ae483e63c65715e0e
will <will.ro.santos@gmail.com>
2020-05-07 00:27:00 -0300

    Update setup.py
    remove c++17

--- setup.py
+++ setup.py
@@ -171,7 +171,8 @@ def cpp_flag(compiler):
     """Return the -std=c++[11/14/17] compiler flag.
     The newer version is preferred over c++11 (when it is available).
     """
-    flags = ["-std=c++17", "-std=c++14", "-std=c++11"]
+    #flags = ["-std=c++17", "-std=c++14", "-std=c++11"]
+    flags = ["-std=c++14", "-std=c++11"]
     if sys.platform == "darwin":
         flags.remove("-std=c++17")  # Pybind11 fails with C++17 on MacOS.
 
